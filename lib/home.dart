fbe25841dfac07fa0fb70ae1d74e187f347dec1e
borgonha <amiltonborgonha.exis@gmail.com>
2022-10-26 23:26:01 -0300

    list post

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+import 'package:consumer_services/post.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
 class Home extends StatefulWidget {
   @override
@@ -7,31 +12,69 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final String _urlBase = 'https://jsonplaceholder.typicode.com';
+
+  static const undefined = 'undefined';
+  @undefined
+  Future<List<Post>> _recuperarPostagens() async {
+    http.Response response = await http.get(_urlBase + '/posts');
+    var dadosJson = jsonDecode(response.body);
+
+    List<Post> postagens = [];
+    for (var post in dadosJson) {
+      Post p =
+          Post(post['_userId'], post['_id'], post['_title'], post['_body']);
+      postagens.add(p);
+    }
+
+    return postagens;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      builder: (context, snapshot) {
-        String? resultado;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Consumer Services'),
+      ),
+      body: FutureBuilder<List<Post>>(
+        future: _recuperarPostagens(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              Center(
+                child: CircularProgressIndicator(),
+              );
 
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            break;
-          case ConnectionState.waiting:
-            print('Connection Waiting....');
-            resultado = 'carregando';
-            break;
-          case ConnectionState.active:
-            break;
-          case ConnectionState.done:
-            if (snapshot.hasError) {
-              resultado = 'Erro ao carregar os dados';
-            } else {}
-            break;
-        }
-        return Center(
-          child: Text(resultado!),
-        );
-      },
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                print('Erro ao carregar as postagens');
+              } else {
+                print('Lista carregada');
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    List<Post> lista = snapshot.data;
+                    Post post = lista?[index];
+
+                    return ListTile(
+                      title: Text(post.title),
+                      subtitle: Text(post!.id.toString()),
+                    );
+                  },
+                );
+              }
+              break;
+          }
+          return Center(
+            child: Text('resultado'),
+          );
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+// @dart=2.9
 import 'package:consumer_services/home.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
     ),
